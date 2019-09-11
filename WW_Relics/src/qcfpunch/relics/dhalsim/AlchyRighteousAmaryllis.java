1837dfa3a63a7b870de66e9329a9d510ef637a81
Clauvin <clauvin_almeida@hotmail.com>
2019-09-11 01:08:35 -0300

    (#526) First version of Righteous Amaryllis relic done
    Now need to take care of edge cases, as "player had 3 potions already, so..."

---
+++ WW_Relics/src/qcfpunch/relics/dhalsim/AlchyRighteousAmaryllis.java
@@ -0,0 +1,52 @@
+package qcfpunch.relics.dhalsim;
+
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.potions.SmokeBomb;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import basemod.abstracts.CustomRelic;
+import qcfpunch.QCFPunch_MiscCode;
+import qcfpunch.resources.relic_graphics.GraphicResources;
+
+public class AlchyRighteousAmaryllis extends CustomRelic {
+
+	public static final String ID = QCFPunch_MiscCode.returnPrefix() +
+			"Alchy_Righteous_Amaryllis";
+	
+	public static int HOW_MANY_ROOMS_TO_GIVE_SMOKE_BOMB = 7;
+	public static float WHICH_MAX_HP_PERCENTAGE_HEALED_WHEN_DEAD = 0.3f;
+	
+	private int rooms_left_to_give_second_smoke_bomb;
+	
+	public AlchyRighteousAmaryllis() {
+		super(ID, GraphicResources.LoadRelicImage("Temp Army Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
+				RelicTier.SHOP, LandingSound.MAGICAL);
+		
+		rooms_left_to_give_second_smoke_bomb = HOW_MANY_ROOMS_TO_GIVE_SMOKE_BOMB;
+	}
+	
+	@Override
+	public void onEquip() {
+		AbstractDungeon.player.obtainPotion(new SmokeBomb());
+	}
+	
+	@Override
+	public void onEnterRoom(AbstractRoom room) {
+		rooms_left_to_give_second_smoke_bomb--;
+		
+		if (rooms_left_to_give_second_smoke_bomb == 0) {
+			AbstractDungeon.player.obtainPotion(new SmokeBomb());
+		}
+	}
+	
+	public boolean canSpawn() {
+		return (Settings.isEndless || AbstractDungeon.floorNum <= 48);
+	}
+	
+	public AbstractRelic makeCopy() {
+		return new AlchyRighteousAmaryllis();
+	}
+
+}
