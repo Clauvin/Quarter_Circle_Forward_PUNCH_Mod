d5c1824e004da2da5a8752e6d423ec4f389136da
Clauvin <clauvin_almeida@hotmail.com>
2019-09-11 22:31:35 -0300

    (#526) Change name of Amaryllis to Cattail

--- WW_Relics/src/qcfpunch/relics/dhalsim/Cattail.java
+++ WW_Relics/src/qcfpunch/relics/dhalsim/Cattail.java
@@ -10,17 +10,17 @@ import basemod.abstracts.CustomRelic;
 import qcfpunch.QCFPunch_MiscCode;
 import qcfpunch.resources.relic_graphics.GraphicResources;
 
-public class AlchyRighteousAmaryllis extends CustomRelic {
+public class Cattail extends CustomRelic {
 
 	public static final String ID = QCFPunch_MiscCode.returnPrefix() +
-			"Alchy_Righteous_Amaryllis";
+			"Cattail";
 	
 	public static int HOW_MANY_ROOMS_TO_GIVE_SMOKE_BOMB = 7;
 	public static float WHICH_MAX_HP_PERCENTAGE_HEALED_WHEN_DEAD = 0.3f;
 	
 	private int rooms_left_to_give_second_smoke_bomb;
 	
-	public AlchyRighteousAmaryllis() {
+	public Cattail() {
 		super(ID, GraphicResources.LoadRelicImage("Temp Army Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.SHOP, LandingSound.MAGICAL);
 		
@@ -46,7 +46,7 @@ public class AlchyRighteousAmaryllis extends CustomRelic {
 	}
 	
 	public AbstractRelic makeCopy() {
-		return new AlchyRighteousAmaryllis();
+		return new Cattail();
 	}
 
 }
