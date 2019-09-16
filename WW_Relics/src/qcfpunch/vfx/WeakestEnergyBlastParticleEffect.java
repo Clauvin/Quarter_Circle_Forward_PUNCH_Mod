c41f6ca5f693354240c73f53499f2faeabfb6782
Clauvin <clauvin_almeida@hotmail.com>
2019-09-15 23:21:29 -0300

    (#533) Name refactoring of WeakestEnergBlastParticleEffect

--- WW_Relics/src/qcfpunch/vfx/WeakestEnergyBlastParticleEffect.java
+++ WW_Relics/src/qcfpunch/vfx/WeakestEnergyBlastParticleEffect.java
@@ -20,18 +20,18 @@ import com.megacrit.cardcrawl.vfx.combat.AdditiveSlashImpactEffect;
 
 public class WeakestEnergyBlastParticleEffect extends AbstractGameEffect {
 
-    private float sX;
-    private float sY;
-    private float tX;
-    private float tY;
-    private float x;
-    private float y;
+    private float starting_point_X;
+    private float starting_point_Y;
+    private float target_X;
+    private float target_Y;
+    private float current_x;
+    private float current_y;
     private float vY;
     private float vX;
     private TextureAtlas.AtlasRegion img;
     private boolean activated;
-    private Color secondaryColor;
-    private float drawScale;
+    private Color secondary_color;
+    private float draw_scale;
     
     public WeakestEnergyBlastParticleEffect(final float sX, final float sY,
     		final float tX, final float tY, final Color ballColor) {
@@ -52,43 +52,43 @@ public class WeakestEnergyBlastParticleEffect extends AbstractGameEffect {
     	
         this.activated = false;
         this.img = ImageMaster.GLOW_SPARK_2;
-        this.sX = sX + MathUtils.random(-90.0f, 90.0f) * Settings.scale;
-        this.sY = sY + MathUtils.random(-90.0f, 90.0f) * Settings.scale;
-        this.tX = tX + MathUtils.random(-50.0f, 50.0f) * Settings.scale;
-        this.tY = tY + MathUtils.random(-50.0f, 50.0f) * Settings.scale;
-        this.vX = this.sX + MathUtils.random(-200.0f, 200.0f) * Settings.scale;
-        this.vY = this.sY + MathUtils.random(-200.0f, 200.0f) * Settings.scale;
-        this.x = this.sX;
-        this.y = this.sY;
+        this.starting_point_X = sX + MathUtils.random(-90.0f, 90.0f) * Settings.scale;
+        this.starting_point_Y = sY + MathUtils.random(-90.0f, 90.0f) * Settings.scale;
+        this.target_X = tX + MathUtils.random(-50.0f, 50.0f) * Settings.scale;
+        this.target_Y = tY + MathUtils.random(-50.0f, 50.0f) * Settings.scale;
+        this.vX = this.starting_point_X + MathUtils.random(-200.0f, 200.0f) * Settings.scale;
+        this.vY = this.starting_point_Y + MathUtils.random(-200.0f, 200.0f) * Settings.scale;
+        this.current_x = this.starting_point_X;
+        this.current_y = this.starting_point_Y;
         this.scale = 0.01f;
         this.startingDuration = 0.8f;
         this.duration = this.startingDuration;
         this.renderBehind = MathUtils.randomBoolean(0.2f);
         this.color = ballColor;
-        this.secondaryColor = secondaryColor;
-        this.drawScale = drawScale;
+        this.secondary_color = secondaryColor;
+        this.draw_scale = drawScale;
     }
     
     @Override
     public void update() {
         if (this.duration > this.startingDuration / 2.0f) {
             this.scale = Interpolation.pow3In.apply(2.5f, this.startingDuration / 2.0f, (this.duration - this.startingDuration / 2.0f) / (this.startingDuration / 2.0f)) * Settings.scale;
-            this.x = Interpolation.swingIn.apply(this.sX, this.vX, (this.duration - this.startingDuration / 2.0f) / (this.startingDuration / 2.0f));
-            this.y = Interpolation.swingIn.apply(this.sY, this.vY, (this.duration - this.startingDuration / 2.0f) / (this.startingDuration / 2.0f));
+            this.current_x = Interpolation.swingIn.apply(this.starting_point_X, this.vX, (this.duration - this.startingDuration / 2.0f) / (this.startingDuration / 2.0f));
+            this.current_y = Interpolation.swingIn.apply(this.starting_point_Y, this.vY, (this.duration - this.startingDuration / 2.0f) / (this.startingDuration / 2.0f));
         }
         else {
             this.scale = Interpolation.pow3Out.apply(2.0f, 2.5f, this.duration / (this.startingDuration / 2.0f)) * Settings.scale;
-            this.x = Interpolation.swingOut.apply(this.tX, this.vX, this.duration / (this.startingDuration / 2.0f));
-            this.y = Interpolation.swingOut.apply(this.tY, this.vY, this.duration / (this.startingDuration / 2.0f));
+            this.current_x = Interpolation.swingOut.apply(this.target_X, this.vX, this.duration / (this.startingDuration / 2.0f));
+            this.current_y = Interpolation.swingOut.apply(this.target_Y, this.vY, this.duration / (this.startingDuration / 2.0f));
         }
         this.duration -= Gdx.graphics.getDeltaTime();
         if (this.duration < this.startingDuration / 2.0f && !this.activated) {
             this.activated = true;
-            this.sX = this.x;
-            this.sY = this.y;
+            this.starting_point_X = this.current_x;
+            this.starting_point_Y = this.current_y;
         }
         if (this.duration < 0.0f) {
-            AbstractDungeon.effectsQueue.add(new AdditiveSlashImpactEffect(this.tX, this.tY, this.color.cpy()));
+            AbstractDungeon.effectsQueue.add(new AdditiveSlashImpactEffect(this.target_X, this.target_Y, this.color.cpy()));
             CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.SHORT, MathUtils.randomBoolean());
             this.isDone = true;
         }
@@ -96,10 +96,10 @@ public class WeakestEnergyBlastParticleEffect extends AbstractGameEffect {
     
     @Override
     public void render(final SpriteBatch sb) {
-        sb.setColor(secondaryColor);
-        sb.draw(this.img, this.x, this.y, this.img.packedWidth / 2.0f, this.img.packedHeight / 2.0f, this.img.packedWidth, this.img.packedHeight, this.scale * (this.drawScale+1f), this.scale * (this.drawScale+1f), this.rotation);
+        sb.setColor(secondary_color);
+        sb.draw(this.img, this.current_x, this.current_y, this.img.packedWidth / 2.0f, this.img.packedHeight / 2.0f, this.img.packedWidth, this.img.packedHeight, this.scale * (this.draw_scale+1f), this.scale * (this.draw_scale+1f), this.rotation);
         sb.setColor(this.color);
-        sb.draw(this.img, this.x, this.y, this.img.packedWidth / 2.0f, this.img.packedHeight / 2.0f, this.img.packedWidth, this.img.packedHeight, this.scale*this.drawScale, this.scale*this.drawScale, this.rotation);
+        sb.draw(this.img, this.current_x, this.current_y, this.img.packedWidth / 2.0f, this.img.packedHeight / 2.0f, this.img.packedWidth, this.img.packedHeight, this.scale*this.draw_scale, this.scale*this.draw_scale, this.rotation);
     }
     
     @Override
