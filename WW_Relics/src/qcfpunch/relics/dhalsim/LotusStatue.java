b29224a9870aa029ae4254f8c17a76a0b9a6bf13
Clauvin <clauvin_almeida@hotmail.com>
2019-09-27 02:05:27 -0300

    (#555) Sleepy version of the Lotus Statue code
    And that means "no, don't count that this code works perfectly".

--- WW_Relics/src/qcfpunch/relics/dhalsim/LotusStatue.java
+++ WW_Relics/src/qcfpunch/relics/dhalsim/LotusStatue.java
@@ -1,18 +1,183 @@
 package qcfpunch.relics.dhalsim;
 
+import com.evacipated.cardcrawl.mod.stslib.relics.ClickableRelic;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnRemoveCardFromMasterDeckRelic;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.CampfireUI;
+import com.megacrit.cardcrawl.rooms.RestRoom;
+import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
+import com.megacrit.cardcrawl.vfx.cardManip.PurgeCardEffect;
+
 import basemod.abstracts.CustomRelic;
 import qcfpunch.QCFPunch_MiscCode;
 import qcfpunch.resources.relic_graphics.GraphicResources;
 
-public class LotusStatue extends CustomRelic {
+public class LotusStatue extends CustomRelic
+							implements OnRemoveCardFromMasterDeckRelic,
+									   ClickableRelic {
 
 	public static final String ID = QCFPunch_MiscCode.returnPrefix() +
 			"Lotus_Statue";
 	
+	private static boolean using_this_relic_power_to_remove = false;
+	private static final int INITIAL_AMOUNT_OF_CHARGES = 0;
+
+	private static boolean right_click_in_relic_here_havent_happened = true;
+	private static boolean currently_choosing_removable_cards;
+	private static int max_amount_of_cards_to_remove;
+	
+	private static final int CAN_SPAWN_BEFORE_FLOOR = 51;
+		
 	public LotusStatue() {
 		super(ID, GraphicResources.LoadRelicImage(
 				"White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.RARE, LandingSound.MAGICAL);
+		
+		this.counter = INITIAL_AMOUNT_OF_CHARGES;
+	}
+	
+	@Override
+	public void onRemoveCardFromMasterDeck(AbstractCard card) {
+		
+		if (!using_this_relic_power_to_remove) this.counter += 1;
+		
+	}
+	
+	public void onEnterRoom(AbstractRoom room) {
+		if ((room instanceof RestRoom) && (counter > 0)) flash();
+		right_click_in_relic_here_havent_happened = false;
+	}
+
+	@Override
+	public void onRightClick() {
+		if (right_click_in_relic_here_havent_happened) {
+			if ((this.counter > 0) && (haveCardsToRemove())) {
+				if (restOptionsHaventBeenPickedUpYet()) {
+					right_click_in_relic_here_havent_happened = false;
+					removingCards();
+				}
+			}
+		}
+	}
+
+	private boolean haveCardsToRemove() {
+		CardGroup removable_cards = AbstractDungeon.player.masterDeck.
+					getPurgeableCards();
+		
+		return removable_cards.size() > 0;
+	}
+	
+	private boolean restOptionsHaventBeenPickedUpYet() {
+		
+		return (AbstractDungeon.getCurrRoom() instanceof RestRoom && 
+				AbstractDungeon.getCurrRoom().phase == 
+					AbstractRoom.RoomPhase.INCOMPLETE &&
+				CampfireUI.hidden == false);
+		
+	}
+	
+	private void removingCards() {
+		
+		AbstractDungeon.dynamicBanner.hide();
+		AbstractDungeon.overlayMenu.cancelButton.hide();
+		AbstractDungeon.previousScreen = AbstractDungeon.screen;
+		
+		AbstractDungeon.getCurrRoom().phase = RoomPhase.INCOMPLETE;
+		
+		CardGroup removable_cards = AbstractDungeon.player.masterDeck.
+				getPurgeableCards();
+		
+		if (removable_cards.size() > this.counter) {
+			max_amount_of_cards_to_remove = this.counter;
+		} else {
+			max_amount_of_cards_to_remove = removable_cards.size();
+		}
+		
+		AbstractDungeon.gridSelectScreen.open(removable_cards,
+				max_amount_of_cards_to_remove,
+				getCardGridDescription(), false, false, true, true);
+		
+		currently_choosing_removable_cards = true;
+		
+	}
+	
+	public void update()
+	{
+		super.update();
+
+		if (currently_choosing_removable_cards) {
+			if (isTimeToRemoveTheChosenCard()) {
+		    
+	            flash();
+				
+				AbstractCard card_chosen = getCardToRemove();
+				
+				using_this_relic_power_to_remove = true;
+				removeAndShowChosenCard(card_chosen);
+				using_this_relic_power_to_remove = false;
+				
+				spendChargesForRemovedCard();
+				
+				AbstractDungeon.gridSelectScreen.selectedCards.clear();
+				
+				AbstractDungeon.overlayMenu.hideBlackScreen();
+				AbstractDungeon.dynamicBanner.appear();
+				AbstractDungeon.isScreenUp = false;
+				
+		    }
+		}
+	}
+	
+	private static boolean isTimeToRemoveTheChosenCard() {
+		
+		boolean i_am_in_a_rest_room = AbstractDungeon.getCurrRoom() instanceof RestRoom;
+		boolean card_to_remove_have_been_chosen = 
+				AbstractDungeon.gridSelectScreen.selectedCards.size() == 1;
+		
+		return currently_choosing_removable_cards && 
+				i_am_in_a_rest_room && card_to_remove_have_been_chosen;
+	}
+	
+	private static AbstractCard getCardToRemove() {
+		return AbstractDungeon.gridSelectScreen.selectedCards.get(0);
+	}
+	
+	private static void removeAndShowChosenCard(AbstractCard chosen_card) {
+    
+		AbstractDungeon.player.masterDeck.removeCard(chosen_card);
+		
+		AbstractDungeon.topLevelEffects.add(
+				new PurgeCardEffect(chosen_card,
+						(Settings.WIDTH / 2),
+						(Settings.HEIGHT / 2)));
+		
+	}
+	
+	private void spendChargesForRemovedCard() {	
+		this.counter -= 1;
+		if (this.counter < 0) this.counter = 0;
+	}
+
+	public String getCardGridDescription() {
+		return DESCRIPTIONS[0];
+	}
+	
+	public String getUpdatedDescription() {
+		return DESCRIPTIONS[0];
+	}
+	
+	public boolean canSpawn() {
+		return (AbstractDungeon.floorNum < CAN_SPAWN_BEFORE_FLOOR) ||
+					(Settings.isEndless);
+	}
+	
+	public AbstractRelic makeCopy() {
+		return new LotusStatue();
 	}
 	
 }
