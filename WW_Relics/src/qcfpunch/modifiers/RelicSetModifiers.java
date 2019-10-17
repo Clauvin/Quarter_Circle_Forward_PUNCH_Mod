e8f9f1cefd6577dead932678bc6ae1452f3a2d8d
Clauvin <clauvin_almeida@hotmail.com>
2019-10-17 18:06:12 -0300

    (#605) Added code for Ascetic Monk's modifier

--- WW_Relics/src/qcfpunch/modifiers/RelicSetModifiers.java
+++ WW_Relics/src/qcfpunch/modifiers/RelicSetModifiers.java
@@ -7,6 +7,9 @@ import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import qcfpunch.QCFP_Misc;
 import qcfpunch.relics.chun_li.*;
+import qcfpunch.relics.dhalsim.BrokenTusk;
+import qcfpunch.relics.dhalsim.LotusStatue;
+import qcfpunch.relics.dhalsim.NecklaceOfSkulls;
 import qcfpunch.relics.guile.*;
 import qcfpunch.relics.ken.BlackTrainingShirt;
 import qcfpunch.relics.ken.RedGi;
@@ -20,24 +23,39 @@ public class RelicSetModifiers {
 
 	//Yes, I know this can be refactored to be a better class, I will do it, bear with me a while.
 	
-	public static final String WANDERING_WARRIOR_ID = QCFP_Misc.returnPrefix() + "WanderingWarrior";
-	public static final String BLUE_JADE_ID = QCFP_Misc.returnPrefix() + "BlueJade";
-	public static final String INDESTRUCTIBLE_FORTRESS_ID = QCFP_Misc.returnPrefix() + "IndestructibleFortress";
-	public static final String BLAZING_FIST_ID = QCFP_Misc.returnPrefix() + "BlazingFist";
-	public static final String RED_CYCLONE_ID = QCFP_Misc.returnPrefix() + "RedCyclone";
+	public static final String WANDERING_WARRIOR_ID =
+			QCFP_Misc.returnPrefix() + "WanderingWarrior";
+	public static final String BLUE_JADE_ID =
+			QCFP_Misc.returnPrefix() + "BlueJade";
+	public static final String INDESTRUCTIBLE_FORTRESS_ID = 
+			QCFP_Misc.returnPrefix() + "IndestructibleFortress";
+	public static final String BLAZING_FIST_ID =
+			QCFP_Misc.returnPrefix() + "BlazingFist";
+	public static final String RED_CYCLONE_ID =
+			QCFP_Misc.returnPrefix() + "RedCyclone";
+	public static final String ASCETIC_MONK_ID =
+			QCFP_Misc.returnPrefix() + "AsceticMonk";
 	
 	public static void addRelicSetModifiers(List<CustomMod> list) {
-		CustomMod wandering_warrior = new CustomMod(RelicSetModifiers.WANDERING_WARRIOR_ID, "y", true);
-		CustomMod blue_jade = new CustomMod(RelicSetModifiers.BLUE_JADE_ID, "y", true);
-		CustomMod indestructible_fortress = new CustomMod(RelicSetModifiers.INDESTRUCTIBLE_FORTRESS_ID, "y", true);
-		CustomMod blazing_fist = new CustomMod(RelicSetModifiers.BLAZING_FIST_ID, "y", true);
-		CustomMod red_cyclone = new CustomMod(RelicSetModifiers.RED_CYCLONE_ID, "y", true);
+		CustomMod wandering_warrior =
+				new CustomMod(RelicSetModifiers.WANDERING_WARRIOR_ID, "y", true);
+		CustomMod blue_jade =
+				new CustomMod(RelicSetModifiers.BLUE_JADE_ID, "y", true);
+		CustomMod indestructible_fortress =
+				new CustomMod(RelicSetModifiers.INDESTRUCTIBLE_FORTRESS_ID, "y", true);
+		CustomMod blazing_fist =
+				new CustomMod(RelicSetModifiers.BLAZING_FIST_ID, "y", true);
+		CustomMod red_cyclone =
+				new CustomMod(RelicSetModifiers.RED_CYCLONE_ID, "y", true);
+		CustomMod ascetic_monk =
+				new CustomMod(RelicSetModifiers.ASCETIC_MONK_ID, "y", true);
 		
 		list.add(wandering_warrior);
 		list.add(blue_jade);
 		list.add(indestructible_fortress);
 		list.add(blazing_fist);
 		list.add(red_cyclone);
+		list.add(ascetic_monk);
 	}
 	
 	public static void addWanderingWarriorRelicsToCustomRun(ArrayList<String> relics) {
@@ -68,6 +86,12 @@ public class RelicSetModifiers {
 		addRelicToCustomRunRelicList(WrestlersCloak.ID, relics);
 		addRelicToCustomRunRelicList(WildHerbsOintment.ID, relics);
 		addRelicToCustomRunRelicList(RedCycloneTeachings.ID, relics);
+	}
+	
+	public static void addAsceticMonkToCustomRun(ArrayList<String> relics) {
+		addRelicToCustomRunRelicList(BrokenTusk.ID, relics);
+		addRelicToCustomRunRelicList(NecklaceOfSkulls.ID, relics);
+		addRelicToCustomRunRelicList(LotusStatue.ID, relics);
 	}
 	
 	public static void addRelicToCustomRunRelicList(String ID, ArrayList<String> relics) {
