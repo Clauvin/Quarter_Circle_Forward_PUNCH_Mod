8c9465a47cc9a15a7d0397a049310ad88685ce41
Clauvin <clauvin_almeida@hotmail.com>
2019-10-18 16:07:19 -0300

    (#610) Mixed Paint Bucket started working a bit

--- WW_Relics/src/qcfpunch/relics/character_cameos/valerie/MixedPaintBucket.java
+++ WW_Relics/src/qcfpunch/relics/character_cameos/valerie/MixedPaintBucket.java
@@ -1,20 +1,15 @@
 package qcfpunch.relics.character_cameos.valerie;
 
-import java.util.ArrayList;
 
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
-import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.colorless.Discovery;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import basemod.abstracts.CustomRelic;
 import qcfpunch.QCFP_Misc;
-import qcfpunch.actions.SetAlwaysRetainOfCardAtCombatAction;
-import qcfpunch.actions.SetExhaustOfCardAtCombatAction;
+import qcfpunch.actions.MixedBucketAction;
 import qcfpunch.resources.relic_graphics.GraphicResources;
-import theArtist.TheArtist;
 import theArtist.cards.PalettePick;
 
 public class MixedPaintBucket extends CustomRelic {
@@ -28,7 +23,11 @@ public class MixedPaintBucket extends CustomRelic {
 				"Temp School Backpack - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.UNCOMMON, LandingSound.CLINK);
 		
-		card_to_give = new PalettePick();
+		if (QCFP_Misc.silentlyCheckForMod(QCFP_Misc.the_artist_class_code)) {
+			card_to_give = new PalettePick();
+		}
+		else card_to_give = new Discovery();
+		
 	}
 	
 	public String getUpdatedDescription() {
@@ -37,42 +36,8 @@ public class MixedPaintBucket extends CustomRelic {
 	
 	@Override
 	public void atBattleStart() {
-		CardGroup hand = AbstractDungeon.player.hand;
-		
-		ArrayList<CardColor> colors = new ArrayList<CardColor>();
-		int amount_of_cards_to_give = 0;
-		
-		for (int i = 0; i < hand.size(); i++) {
-			CardColor color = hand.getNCardFromTop(i).color;
-			boolean new_color = true;
-			for (int j = 0; j < colors.size(); j++) {
-				if (colors.get(j) == color) {
-					new_color = false;
-					break;
-				}
-			}
-			if (new_color) {
-				colors.add(color);
-				amount_of_cards_to_give += 1;
-			}
-		}
-				
-		for (int i = 0; i < amount_of_cards_to_give; i++) {
-			AbstractCard given_card = card_to_give.makeStatEquivalentCopy();
-			
-			QCFP_Misc.setCardToAlwaysRetain(
-					given_card, true);
-			
-			QCFP_Misc.reduceCardCostIfNotStatusOrCurseByOne(given_card);
-			
-			AbstractDungeon.actionManager.addToBottom(
-					new MakeTempCardInHandAction(given_card));
-			
-			AbstractDungeon.actionManager.addToBottom(
-					new SetExhaustOfCardAtCombatAction(given_card.uuid, true));
-		}
-		
-		
+		AbstractDungeon.actionManager.addToBottom(
+				new MixedBucketAction(card_to_give));
 	}
 	
 	public boolean canSpawn() {
