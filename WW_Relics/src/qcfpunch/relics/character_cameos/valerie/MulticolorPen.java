e0341d031b34b9b73332c6021dacdb87e886a27d
Clauvin <clauvin_almeida@hotmail.com>
2019-10-18 18:02:46 -0300

    (#612) Created skeleton class for Multicolor Pen

---
+++ WW_Relics/src/qcfpunch/relics/character_cameos/valerie/MulticolorPen.java
@@ -0,0 +1,32 @@
+package qcfpunch.relics.character_cameos.valerie;
+
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import basemod.abstracts.CustomRelic;
+import qcfpunch.QCFP_Misc;
+import qcfpunch.resources.relic_graphics.GraphicResources;
+
+public class MulticolorPen extends CustomRelic  {
+
+	public static final String ID = QCFP_Misc.returnPrefix() + "Multicolor_Pen";
+	
+	public MulticolorPen() {
+		super(ID, GraphicResources.LoadRelicImage(
+				"Temp School Backpack - steeltoe-boots - Lorc - CC BY 3.0.png"),
+				RelicTier.RARE, LandingSound.CLINK);
+	}
+	
+	public String getUpdatedDescription() {
+		return DESCRIPTIONS[0];
+	}
+	
+	public boolean canSpawn() {
+		return QCFP_Misc.checkForMod(QCFP_Misc.the_artist_class_code);
+	}
+	
+	
+	public AbstractRelic makeCopy() { // always override this method to return a new instance of your relic
+		return new MixedPaintBucket();
+	}
+	
+}
