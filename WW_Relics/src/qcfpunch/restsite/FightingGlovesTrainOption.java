8b53ad907bc9ff3e08c394be19aad996ef0df931
Clauvin <clauvin_almeida@hotmail.com>
2019-10-22 00:32:12 -0300

    (#449) Created empty FightingGlovesTrainOption

---
+++ WW_Relics/src/qcfpunch/restsite/FightingGlovesTrainOption.java
@@ -0,0 +1,13 @@
+package qcfpunch.restsite;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
+import com.megacrit.cardcrawl.ui.campfire.SmithOption;
+
+public class FightingGlovesTrainOption extends AbstractCampfireOption {
+
+	public void useOption() {
+		//AbstractDungeon.effectList.add(); 
+	}
+	
+}
