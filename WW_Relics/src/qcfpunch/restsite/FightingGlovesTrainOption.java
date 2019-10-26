acb16592a686ad4c494b77e9480d0aa41712044e
Clauvin <clauvin_almeida@hotmail.com>
2019-10-26 01:53:16 -0300

    (#449) FGlovesTrainOption now can be created usable or not

--- WW_Relics/src/qcfpunch/restsite/FightingGlovesTrainOption.java
+++ WW_Relics/src/qcfpunch/restsite/FightingGlovesTrainOption.java
@@ -2,10 +2,14 @@ package qcfpunch.restsite;
 
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
-import com.megacrit.cardcrawl.ui.campfire.SmithOption;
 
 public class FightingGlovesTrainOption extends AbstractCampfireOption {
 
+	public FightingGlovesTrainOption(boolean usable) {
+		super();
+		this.usable = usable;
+	}
+	
 	public void useOption() {
 		AbstractDungeon.effectList.add(new FightingGlovesTrainEffect());
 	}
