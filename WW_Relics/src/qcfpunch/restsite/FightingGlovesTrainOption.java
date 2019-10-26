6d5dc0a88ef8a460f487b8fda50f9288e267aee6
Clauvin <clauvin_almeida@hotmail.com>
2019-10-26 02:34:46 -0300

    (#449) ...GODDAMMIT JDGUI!!! Anyway, now FGlovesTrainOption should break less

--- WW_Relics/src/qcfpunch/restsite/FightingGlovesTrainOption.java
+++ WW_Relics/src/qcfpunch/restsite/FightingGlovesTrainOption.java
@@ -1,17 +1,28 @@
 package qcfpunch.restsite;
 
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
 
 public class FightingGlovesTrainOption extends AbstractCampfireOption {
 
+    private static final UIStrings uiStrings;
+    public static final String[] TEXT;
+	
 	public FightingGlovesTrainOption(boolean usable) {
-		super();
+		this.label = FightingGlovesTrainOption.TEXT[0];
+        this.description = FightingGlovesTrainOption.TEXT[1];
+        this.img = null;
 		this.usable = usable;
 	}
 	
 	public void useOption() {
 		AbstractDungeon.effectList.add(new FightingGlovesTrainEffect());
 	}
-	
+    
+    static {
+        uiStrings = CardCrawlGame.languagePack.getUIString("Train Option");
+        TEXT = FightingGlovesTrainOption.uiStrings.TEXT;
+    }
 }
