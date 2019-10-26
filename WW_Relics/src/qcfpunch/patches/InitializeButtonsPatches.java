91a357deba955702f801fb778e0c279dc6e65fba
Clauvin <clauvin_almeida@hotmail.com>
2019-10-26 01:53:46 -0300

    (#449) Hoping this commit won't break the mod thanks to FGlovesTrainOption not having an image

--- WW_Relics/src/qcfpunch/patches/InitializeButtonsPatches.java
+++ WW_Relics/src/qcfpunch/patches/InitializeButtonsPatches.java
@@ -6,10 +6,10 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.CampfireUI;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
-import com.megacrit.cardcrawl.ui.campfire.SmithOption;
 
 import basemod.ReflectionHacks;
 import qcfpunch.relics.ryu.FightingGloves;
+import qcfpunch.restsite.FightingGlovesTrainOption;
 
 //Yes, base from Vex's The Artificer's InitializeButtonsPatches.
 @SpirePatch(cls = "com.megacrit.cardcrawl.rooms.CampfireUI", method = "initializeButtons")
@@ -24,10 +24,10 @@ public class InitializeButtonsPatches
 	    	if (AbstractDungeon.player.hasRelic("qcfpunch:Fighting_Gloves")) {
 	    		FightingGloves gloves = (FightingGloves) AbstractDungeon.player.getRelic("qcfpunch:Fighting_Gloves");
 	    		if (gloves.shouldTheRelicBeUsedNow()) {
-	    			campfireButtons.add(new SmithOption(true));
+	    			campfireButtons.add(new FightingGlovesTrainOption(true));
 	    		}
 	    		else {
-	    			campfireButtons.add(new SmithOption(false));
+	    			campfireButtons.add(new FightingGlovesTrainOption(false));
 	    		}
 	    		
 	    	}
