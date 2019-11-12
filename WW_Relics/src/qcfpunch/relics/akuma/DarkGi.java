aa1bb0b25931d1390132f685046bd5e15c097e4b
Clauvin <clauvin_almeida@hotmail.com>
2019-11-11 23:58:31 -0300

    (#642) Dark Gi's relic effects ready for testing

--- WW_Relics/src/qcfpunch/relics/akuma/DarkGi.java
+++ WW_Relics/src/qcfpunch/relics/akuma/DarkGi.java
@@ -1,5 +1,9 @@
 package qcfpunch.relics.akuma;
 
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import basemod.abstracts.CustomRelic;
@@ -9,6 +13,10 @@ import qcfpunch.resources.relic_graphics.GraphicResources;
 public class DarkGi extends CustomRelic {
 	
 	public static final String ID = QCFP_Misc.returnPrefix() + "Dark_Gi";
+	
+	public static final float PERCENTAGE_OF_MAX_HP_TO_LOSE = 0.3f;
+	public static final float HEAL_EFFECTIVENESS = 0.5f;
+	public static final int AMOUNT_TO_DRAW_WHEN_A_CARD_IS_PLAYED = 1;
 	
 	public DarkGi() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
@@ -21,8 +29,27 @@ public class DarkGi extends CustomRelic {
 	
 	@Override
 	public void onEquip() {
-		// TODO Auto-generated method stub
-		super.onEquip();
+		int max_hp_to_lose = (int) Math.floor(AbstractDungeon.player.maxHealth * 
+				PERCENTAGE_OF_MAX_HP_TO_LOSE);
+		
+		AbstractDungeon.player.decreaseMaxHealth(max_hp_to_lose);
+		
+		AbstractDungeon.player.gameHandSize += 1;
+	}
+	
+	@Override
+	public int onPlayerHeal(int healAmount) {
+		int amount_to_heal = (int)Math.floor(healAmount * HEAL_EFFECTIVENESS);
+		
+		return amount_to_heal;
+	}
+	
+	@Override
+	public void onPlayCard(AbstractCard c, AbstractMonster m) {
+		super.onPlayCard(c, m);
+		AbstractDungeon.actionManager.addToBottom(
+				new DrawCardAction(AbstractDungeon.player,
+						AMOUNT_TO_DRAW_WHEN_A_CARD_IS_PLAYED));
 	}
 	
 	@Override
