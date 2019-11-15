d538f9567d46172d86a622ac7a7e4e59de3916f4
Clauvin <clauvin_almeida@hotmail.com>
2019-11-15 02:10:43 -0300

    (#642) Dark Gi is now simplified

--- WW_Relics/src/qcfpunch/relics/akuma/DarkGi.java
+++ WW_Relics/src/qcfpunch/relics/akuma/DarkGi.java
@@ -6,7 +6,6 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
-import basemod.BaseMod;
 import basemod.abstracts.CustomRelic;
 import qcfpunch.QCFP_Misc;
 import qcfpunch.resources.relic_graphics.GraphicResources;
@@ -15,11 +14,8 @@ public class DarkGi extends CustomRelic {
 	
 	public static final String ID = QCFP_Misc.returnPrefix() + "Dark_Gi";
 	
-	public static final float PERCENTAGE_OF_MAX_HP_TO_LOSE = 0.25f;
-	public static final float HEAL_EFFECTIVENESS = 0.5f;
+	public static final float PERCENTAGE_OF_MAX_HP_TO_LOSE = 0.35f;
 	public static final int AMOUNT_TO_DRAW_WHEN_A_CARD_IS_PLAYED = 1;
-	public static final int EXTRA_DRAW_EACH_TURN = 1;
-	public static final int EXTRA_HAND_SIZE = 1;
 	
 	public DarkGi() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
@@ -36,19 +32,7 @@ public class DarkGi extends CustomRelic {
 				PERCENTAGE_OF_MAX_HP_TO_LOSE);
 		
 		AbstractDungeon.player.decreaseMaxHealth(max_hp_to_lose);
-		
-		BaseMod.MAX_HAND_SIZE += EXTRA_HAND_SIZE;
-		AbstractDungeon.player.masterHandSize += EXTRA_DRAW_EACH_TURN;
 	}
-	
-	@Override
-	public int onPlayerHeal(int healAmount) {
-		int amount_to_heal = (int)Math.floor(healAmount * HEAL_EFFECTIVENESS);
-		
-		return amount_to_heal;
-	}
-	
-	
 	
 	@Override
 	public void onPlayCard(AbstractCard c, AbstractMonster m) {
