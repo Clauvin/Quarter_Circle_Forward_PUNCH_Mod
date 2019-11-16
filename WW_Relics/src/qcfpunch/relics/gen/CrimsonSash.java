3cf944e7612e863683ff73eb0ecb95a6e8d408fd
Clauvin <clauvin_almeida@hotmail.com>
2019-11-16 00:53:49 -0300

    (#642) Crimson Sash done, I suppose

---
+++ WW_Relics/src/qcfpunch/relics/gen/CrimsonSash.java
@@ -0,0 +1,43 @@
+package qcfpunch.relics.gen;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import basemod.abstracts.CustomRelic;
+import qcfpunch.QCFP_Misc;
+import qcfpunch.resources.relic_graphics.GraphicResources;
+
+public class CrimsonSash extends CustomRelic  {
+
+	public static final String ID = QCFP_Misc.returnPrefix() + "Crimson_Sash";
+	
+	public static final float HEALING_LOWER_EFFICIENCY = 0.5f;
+	public static final int MORE_ENERGY_YAY = 1;
+	
+	public CrimsonSash() {
+		super(ID, GraphicResources.LoadRelicImage("Temp Army Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
+				RelicTier.BOSS, LandingSound.FLAT);
+	}
+	
+	public String getUpdatedDescription() { return this.DESCRIPTIONS[0]; }
+	
+	public void onEquip() {
+		AbstractDungeon.player.energy.energyMaster += MORE_ENERGY_YAY;
+	}
+
+	public void onUnequip() {
+		AbstractDungeon.player.energy.energyMaster -= MORE_ENERGY_YAY;
+	}
+	
+	@Override
+	public int onPlayerHeal(int healAmount) {
+		int true_heal_amount = Math.round(healAmount * HEALING_LOWER_EFFICIENCY); 
+		
+		return super.onPlayerHeal(true_heal_amount);
+	}
+	
+	public AbstractRelic makeCopy() {
+		return new CrimsonSash();
+	}
+	
+}
