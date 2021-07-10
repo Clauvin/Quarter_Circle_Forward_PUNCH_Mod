c5dac93d094d5bcfd9f3c37b6a2c82087abe9dbe
Cláuvin <Clauvin@users.noreply.github.com>
2021-07-10 00:54:51 -0300

    (#698) The best I could do without patching
    Basically, what I did was making stuff work using the code that's already there, in terms of making the Emerald flames work, and activating the EmeraldBuff function.
    
    The main problem is that there are moments where it does not work and or a) I find a way to make it work piggybacking on functions that AbstractRoom has (possible)
    
    OR
    
    I start patching. A problem for later.

--- WW_Relics/src/qcfpunch/rooms/MonsterRoomEmeraldElite.java
+++ WW_Relics/src/qcfpunch/rooms/MonsterRoomEmeraldElite.java
@@ -1,12 +1,21 @@
 package qcfpunch.rooms;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.unique.IncreaseMaxHpAction;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ModHelper;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
+import com.megacrit.cardcrawl.powers.RegenerateMonsterPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.relics.AbstractRelic.RelicTier;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
+import qcfpunch.QCFP_Misc;
+
+import java.util.Iterator;
 
 public class MonsterRoomEmeraldElite extends MonsterRoomElite {
 
@@ -21,9 +30,12 @@ public class MonsterRoomEmeraldElite extends MonsterRoomElite {
 	    else
 	    {
 	    	addRelicToRewards(tier);
+			tier = returnRandomRelicTier();
+			addRelicToRewards(tier);
 	    	if (AbstractDungeon.player.hasRelic("Black Star")) {
-	        addNoncampRelicToRewards(returnRandomRelicTier());
-	      }
+				addNoncampRelicToRewards(returnRandomRelicTier());
+				addNoncampRelicToRewards(returnRandomRelicTier());
+	      	}
 
 	    	addEmeraldKey();
 	    }
@@ -53,6 +65,51 @@ public class MonsterRoomEmeraldElite extends MonsterRoomElite {
 	    	return AbstractRelic.RelicTier.RARE;
 	    }
 	    return AbstractRelic.RelicTier.UNCOMMON;
+	}
+
+	public void applyEmeraldEliteBuff(){
+		//Why the weird var1 iterator?
+		// Because I copied this from MonsterRoomElite when decompiled on Intelij,
+		// while trying to make this overriding function work :/
+		Iterator var1;
+		AbstractMonster m;
+		switch(AbstractDungeon.mapRng.random(0, 3)) {
+			case 0:
+				var1 = this.monsters.monsters.iterator();
+
+				while(var1.hasNext()) {
+					m = (AbstractMonster)var1.next();
+					AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, m, new StrengthPower(m, AbstractDungeon.actNum + 1), AbstractDungeon.actNum + 1));
+				}
+
+				return;
+			case 1:
+				var1 = this.monsters.monsters.iterator();
+
+				while(var1.hasNext()) {
+					m = (AbstractMonster)var1.next();
+					AbstractDungeon.actionManager.addToBottom(new IncreaseMaxHpAction(m, 0.25F, true));
+				}
+
+				return;
+			case 2:
+				var1 = this.monsters.monsters.iterator();
+
+				while(var1.hasNext()) {
+					m = (AbstractMonster)var1.next();
+					AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, m, new MetallicizePower(m, AbstractDungeon.actNum * 2 + 2), AbstractDungeon.actNum * 2 + 2));
+				}
+
+				return;
+			case 3:
+				var1 = this.monsters.monsters.iterator();
+
+				while(var1.hasNext()) {
+					m = (AbstractMonster)var1.next();
+					AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, m, new RegenerateMonsterPower(m, 1 + AbstractDungeon.actNum * 2), 1 + AbstractDungeon.actNum * 2));
+				}
+		}
+		QCFP_Misc.fastLoggerLine(AbstractDungeon.actionManager.actions.size());
 	}
 	
 }
