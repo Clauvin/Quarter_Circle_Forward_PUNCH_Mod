e3a21b0a946a8ac5d7af710a18c99598ff112818
Cláuvin <Clauvin@users.noreply.github.com>
2021-07-22 18:21:15 -0300

    (#671) Refactored DuffelBagCardReward into DuffelBagBandageCardReward

---
+++ WW_Relics/src/qcfpunch/DuffelBagBandageCardReward.java
@@ -0,0 +1,35 @@
+package qcfpunch;
+
+import basemod.abstracts.CustomReward;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.colorless.BandageUp;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import qcfpunch.resources.relic_graphics.GraphicResources;
+
+import java.util.ArrayList;
+
+public class DuffelBagBandageCardReward extends CustomReward {
+
+    public static final String[] TEXT = (CardCrawlGame.languagePack.
+            getUIString(QCFP_Misc.returnPrefix() + "Duffel_Bag")).TEXT;
+
+    public DuffelBagBandageCardReward() {
+        super(GraphicResources.LoadRelicImage("Duffel_Bag - swap-bag - Lorc - CC BY 3.0.png"),
+                "", RewardType.CARD);
+        this.cards = new ArrayList<AbstractCard>();
+        this.cards.add(new BandageUp());
+        this.text = TEXT[3] + TEXT[5] + TEXT[4];
+    }
+
+    @Override
+    public boolean claimReward() {
+        if (AbstractDungeon.screen == AbstractDungeon.CurrentScreen.COMBAT_REWARD) {
+            AbstractDungeon.cardRewardScreen.open(this.cards, this, TEXT[4]);
+            AbstractDungeon.previousScreen = AbstractDungeon.CurrentScreen.COMBAT_REWARD;
+        }
+
+        return false;
+    }
+}
