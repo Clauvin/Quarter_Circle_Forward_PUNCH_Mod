2e5f9eda16920acfaab66b47539b03a4bfdc3265
Cláuvin <Clauvin@users.noreply.github.com>
2021-07-23 06:49:00 -0300

    (#671) Created LoadRewardImage(String)

--- WW_Relics/src/qcfpunch/resources/relic_graphics/GraphicResources.java
+++ WW_Relics/src/qcfpunch/resources/relic_graphics/GraphicResources.java
@@ -8,13 +8,15 @@ public class GraphicResources {
 	public static final String GRAPHIC_RESOURCES_ADDRESS = "qcfpunch/resources/";
 	public static final String RELIC_GRAPHICS_SUBADDRESS = "relic_graphics/";
 	public static final String RELIC_GRAPHICS_OUTLINES_SUBADDRESS = "outlines/";
+	public static final String REWARD_GRAPHICS_SUBADDRESS = "reward_graphics/";
 	public static final String RELIC_TEXTURE_ADDRESS =
 			GRAPHIC_RESOURCES_ADDRESS + RELIC_GRAPHICS_SUBADDRESS;
 	public static final String RELIC_OUTLINE_TEXTURE_ADDRESS =
 			GRAPHIC_RESOURCES_ADDRESS + RELIC_GRAPHICS_SUBADDRESS +
 				RELIC_GRAPHICS_OUTLINES_SUBADDRESS;
-	
-	
+	public static final String REWARD_TEXTURE_ADDRESS =
+			GRAPHIC_RESOURCES_ADDRESS + REWARD_GRAPHICS_SUBADDRESS;
+
 	public static Texture img_red_headband;
 	public static Texture bg_img_red_headband;
 	
@@ -26,6 +28,12 @@ public class GraphicResources {
 	public static Texture LoadOutlineImage(String outline_filename) {
 		return ImageMaster.loadImage(GraphicResources.RELIC_OUTLINE_TEXTURE_ADDRESS +
 				outline_filename);
+	}
+
+	public static Texture LoadRewardImage(String reward_filename){
+		return ImageMaster.loadImage(GraphicResources.REWARD_TEXTURE_ADDRESS +
+				reward_filename);
+
 	}
 	
 }
