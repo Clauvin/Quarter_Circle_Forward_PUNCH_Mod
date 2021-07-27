93d298be08b9c56f8a145ee642530a9f328057e8
Cláuvin <Clauvin@users.noreply.github.com>
2021-07-27 08:33:04 -0300

    (#671) Fixed Reward Type patch

--- WW_Relics/src/qcfpunch/patches/RewardItemTypeEnumPatch.java
+++ WW_Relics/src/qcfpunch/patches/RewardItemTypeEnumPatch.java
@@ -1,11 +1,14 @@
 package qcfpunch.patches;
 
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 
 public class RewardItemTypeEnumPatch  {
 
+    @SpireEnum
     public static RewardItem.RewardType DUFFELBAGBANDAGECARDREWARD;
 
+    @SpireEnum
     public static RewardItem.RewardType DUFFELBAGPANACEACARDREWARD;
 
 
