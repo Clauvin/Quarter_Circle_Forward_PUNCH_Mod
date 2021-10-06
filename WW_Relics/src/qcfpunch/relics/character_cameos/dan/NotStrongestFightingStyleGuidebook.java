4da2158c7813fee41c6e15794b2608a78b7fb3a0
Cláuvin <Clauvin@users.noreply.github.com>
2021-10-05 22:27:38 -0300

    (#273) Added sound to NotStrongestFightingStyle

--- WW_Relics/src/qcfpunch/relics/character_cameos/dan/NotStrongestFightingStyleGuidebook.java
+++ WW_Relics/src/qcfpunch/relics/character_cameos/dan/NotStrongestFightingStyleGuidebook.java
@@ -2,7 +2,9 @@ package qcfpunch.relics.character_cameos.dan;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.audio.Sfx;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
@@ -15,10 +17,13 @@ import qcfpunch.resources.relic_graphics.GraphicResources;
 public class NotStrongestFightingStyleGuidebook extends CustomRelic {
 
 	public static final String ID = QCFP_Misc.returnPrefix() + "Not_Strongest_Fighting_Style_Guidebook";
+	public static final String on_equip_sound_file_name = "Strongest - se_mid_00033.wav";
+	public static Sfx strongest_sound;
 	
 	public NotStrongestFightingStyleGuidebook() {
 		super(ID, GraphicResources.LoadRelicImage("Temp Strongest Style Manual - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.COMMON, LandingSound.FLAT);
+		strongest_sound = new Sfx("qcfpunch/resources/sounds/" + on_equip_sound_file_name, false);
 	}
 	
 	public String getUpdatedDescription() {
@@ -40,6 +45,7 @@ public class NotStrongestFightingStyleGuidebook extends CustomRelic {
 	@Override
 	public void onEquip() {
 		BaseMod.MAX_HAND_SIZE++;
+		strongest_sound.play(Settings.MASTER_VOLUME * Settings.SOUND_VOLUME, 1, 0);
 	}
 	
 	@Override
